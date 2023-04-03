72fe5dbc1c53b0b8da0773fa0385bd1d9f2428c1
rrvspk123 <112676711+rrvspk123@users.noreply.github.com>
2023-04-03 03:24:20 +0000

    routes update

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,9 @@
-from datetime import datetime
+
 from flask import render_template, flash, redirect, url_for, request, g
-from flask_login import login_user, logout_user, current_user, login_required
-from werkzeug.urls import url_parse
-from flask_babel import _, get_locale
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, \
-    ResetPasswordRequestForm, ResetPasswordForm
-from app.models import User, Post
-from app.email import send_password_reset_email
+
+
+
 
 
 
